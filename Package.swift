87ce5265db5bec203ea400938367865dc5c548c6
cosmocolor <153699447+cosmocolor@users.noreply.github.com>
2024-04-22 12:20:50 -0600

    Update Package.swift
    cambio de versión

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(name: "ISOMDLLib", 
                       url: "https://storage.googleapis.com/cosmo-ios-repo/ISOMDLLib.xcframework.zip", 
-                      checksum: "0c209995f17270dc09cabe440f27ba5d52f3db859264dd45eef13e350fa606d8"
+                      checksum: "074bcfb280d9b090a122c9bd58b152135d87054b123176cefa8650b83089fc33"
                      )
     ]
 )
