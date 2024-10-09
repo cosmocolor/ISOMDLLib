308f676849e998b9e34f5a059ddc1cf16d476fc7
jcortezstudio <64922305+jcortezstudio@users.noreply.github.com>
2024-10-09 10:28:08 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,17 +12,12 @@ let package = Package(
             name: "ISOMDLLib",
             targets: ["ISOMDLLib"])
     ],
-    dependencies: [
-        .package(url: "https://github.com/valpackett/SwiftCBOR.git", from: "0.4.6"),
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.9.1"),
-        .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.15.3")
-    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(name: "ISOMDLLib", 
-                      url: "https://storage.googleapis.com/cosmo-ios-repo/ISOMDLLib.xcframework.zip", 
-                      checksum: "9f50742882737ac3501d168dd6dc6556536e04ea528daa1fd890c13289411671"
+                      url: "https://storage.googleapis.com/cosmo-ios-repo/ISOMDLLib-894297.xcframework.zip", 
+                      checksum: "1e33c97966bd3637f1d7d0e63170e21da16bde6acd7c3178d73d242d9ef0f0f4"
                      )
         
     ],
