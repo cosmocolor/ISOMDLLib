45bb4bf2828bdf7b427821612eff74d63689af0a
jcortez <jcortez@cosmocolor.com.mx>
2025-10-20 11:51:50 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(name: "ISOMDLLib", 
-                      url: "https://storage.googleapis.com/cosmo-ios-repo/ISOMDLLib-d5f1d7.xcframework.zip", 
-                      checksum: "ebf733f055750ce5f50533d0dfbfdf986fdc5d75937cb73e215be9651fa4becb"
+                      url: "https://storage.googleapis.com/cosmo-ios-repo/ISOMDLLib-d76965.xcframework.zip", 
+                      checksum: "257b35ae15e9791e8e26be64b5bff610db5c299ca6fd9299130649a781966dde"
                      )
         
     ],
